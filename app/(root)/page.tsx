0d0714924dcd915efe4cb1d75a819311ded0c751
abderrahmanebennaceur <abderrahmane.bennaceur.213@gmail.com>
2025-01-02 00:31:27 +0100

    feat: call components in app root

---
+++ app/(root)/page.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import HeroSection from '../../components/Hero'
+import LatestAddedBooks from '../../components/LatestAddedBooks'
+import SocialProofSection from '../../components/SocialProof'
+import TestimonialSection from '../../components/Testimonial'
+import FAQSection from '../../components/FAQ'
+import CtaSection from '@/components/CTA'
+import FeaturesSection from '@/components/Features'
+import HowItWorksSection from '@/components/HowItWorks'
+
+function Home() {
+  return (
+    <>
+      <HeroSection />
+      <SocialProofSection />
+      <LatestAddedBooks />
+      <FeaturesSection />
+      <HowItWorksSection />
+      <TestimonialSection />
+      <CtaSection />
+      <FAQSection />
+    </>
+  )
+}
+
+export default Home
